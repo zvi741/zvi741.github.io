0831814f36a1c104f7e97287dc7ff162ae596098
zvi741 <51421153+zvi741@users.noreply.github.com>
2019-08-28 20:47:57 +0300

    First Commit

---
+++ javascript/app.js
@@ -0,0 +1,19 @@
+var nav_is_open = false;
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    menu_icon = document.getElementById("menu-icon");
+    menu_icon.addEventListener("click", function () {
+        if (nav_is_open == false) {
+            document.getElementById("nav").style.transform = "translate(0, 0)";
+            nav_is_open = true;
+            console.log("opened");
+        } else {
+            document.getElementById("nav").style.transform = "translate(0, -500px)";
+            nav_is_open = false;
+            console.log("closed");
+        }
+    });
+
+});
+
