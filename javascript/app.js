4e1cf2326ea4442768703cca240129430d747645
zvi741 <51421153+zvi741@users.noreply.github.com>
2019-08-29 10:48:07 +0300

    2nd Commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,28 @@
 var nav_is_open = false;
 
 document.addEventListener("DOMContentLoaded", function (event) {
+    navOpener();
+});
 
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+function navOpener() {
     menu_icon = document.getElementById("menu-icon");
     menu_icon.addEventListener("click", function () {
+
         if (nav_is_open == false) {
+
             document.getElementById("nav").style.transform = "translate(0, 0)";
             nav_is_open = true;
             console.log("opened");
+
         } else {
+
             document.getElementById("nav").style.transform = "translate(0, -500px)";
             nav_is_open = false;
             console.log("closed");
         }
     });
-
-});
-
+}
